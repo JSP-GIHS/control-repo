746fc56ba540ab0ff79350c8e9b11f76b37b351e
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-04-19 11:46:57 +0930

    Add note to jenkins role because I keep forgetting to modify the hiera for it

--- site/role/manifests/jenkins.pp
+++ site/role/manifests/jenkins.pp
@@ -1,5 +1,9 @@
 #configure the jenkins role
 class role::jenkins {
+  # be sure to add
+  # mcollective::client: true
+  # to the hiera for the nodes that you apply
+  # this role to.
   include profile::base
   include profile::jenkins
 }
