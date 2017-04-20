f5524ea840ba7294e8eb3cdb305e5abd32d2f9f3
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-04-20 10:14:04 +0930

    applicationtier: codeservers

---
+++ site/profile/manifests/codeserver.pp
@@ -0,0 +1,24 @@
+# Code hosting services (specifically jenkins and gitweb)
+class profile::codeserver {
+
+  File {
+    owner => 'root',
+    group => 'root',
+  }
+
+  file { ['/etc/puppetlabs/facter', '/etc/puppetlabs/facter/facts.d']:
+    ensure => 'directory',
+  }
+
+  file { '/etc/puppetlabs/facter/facts.d/facts.yaml':
+    ensure => 'present',
+  }
+
+  file_line { 'applicationtier_codeservers':
+    ensure => 'present',
+    path   => '/etc/puppetlabs/facter/facts.d/facts.yaml',
+    line   => 'applicationtier: codeservers',
+    match  => '^applicationtier:',
+  }
+
+}
