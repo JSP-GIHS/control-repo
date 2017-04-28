ca6162d12e232eff02d15a1c989cd63f1f9fd2fa
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-04-28 22:17:10 +0930

    Better DNS module, and included searchdomain so as not to break everything horribly

--- site/dns/manifests/init.pp
+++ site/dns/manifests/init.pp
@@ -1,9 +1,11 @@
 # Simple DNS module. Configure with auto-hiera
+# dns::searchdomain: "dev.internal"
 # dns::nameservers:
 #   - 192.168.10.10
 #   - 192.168.10.11
 class dns (
-  $nameservers = "192.168.10.10"
+  $nameservers = "192.168.10.10",
+  $searchdomain = "dev.internal",
 ) {
 
   file { '/etc/resolv.conf':
