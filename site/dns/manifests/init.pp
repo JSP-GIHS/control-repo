66972e6a52af9b1b6c34b5a951228a2a065ea088
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-05-01 09:01:37 +0930

    puppet-lint variable quotation cleanup

--- site/dns/manifests/init.pp
+++ site/dns/manifests/init.pp
@@ -4,8 +4,8 @@
 #   - 192.168.10.10
 #   - 192.168.10.11
 class dns (
-  $nameservers = "192.168.10.10",
-  $searchdomain = "dev.internal",
+  $nameservers = '192.168.10.10',
+  $searchdomain = 'dev.internal',
 ) {
 
   file { '/etc/resolv.conf':
