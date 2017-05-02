1f1a53473e44a044044cda8d41edebfe6136ca3d
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-05-02 15:43:58 +0930

    Correctly handle nagios hostgroups for export class

--- site/nagios/manifests/export.pp
+++ site/nagios/manifests/export.pp
@@ -6,23 +6,22 @@ class nagios::export (
 ) {
 
   if $::osfamily == 'windows' {
-    $templatetype = 'windows'
+    $templatetype = 'windows-server'
   } else {
-    $templatetype = 'linux'
+    $templatetype = 'linux-server'
   }
 
-  $hostgroup = ''
   if $hostgroups != '' {
-    $hostgroups.each |String $hgname| {
-      $hostgroup = "${hostgroup},${hgname}"
-    }
+    $hostgroup = join(["${templatetype}s", join($hostgroups, ',')], ',')
+  } else {
+    $hostgroup = "${templatetype}s"
   }
 
   nagios::resource { $::fqdn:
     type          => 'host',
     address       => $::ipaddress,
-    hostgroups    => "${templatetype}-servers${hostgroup}",
-    host_use      => "${templatetype}-server",
+    hostgroups    => $hostgroup,
+    host_use      => $templatetype,
     check_command => 'check_ping!2000.00,80%!4000.00,100%!4',
     exported      => true,
   }
