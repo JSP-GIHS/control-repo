1ebd8f5a02046d743c690d58a97dc6138754cc95
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-05-02 13:09:06 +0930

    Add hostgroup to each host from hiera

--- site/nagios/manifests/export.pp
+++ site/nagios/manifests/export.pp
@@ -1,7 +1,9 @@
 # Nagios export class
 #
 # Used to generate the hosts configuration
-class nagios::export {
+class nagios::export (
+  $hostgroups = '',
+) {
 
   if $::osfamily == 'windows' {
     $templatetype = 'windows'
@@ -9,13 +11,29 @@ class nagios::export {
     $templatetype = 'linux'
   }
 
+  $hostgroup = ''
+  if $hostgroups != '' {
+    $hostgroups.each |String $hgname| {
+      $hostgroup = "${hostgroup},${hgname}"
+    }
+  }
+
   nagios::resource { $::fqdn:
     type          => 'host',
     address       => $::ipaddress,
-    hostgroups    => "${templatetype}-servers",
+    hostgroups    => "${templatetype}-servers${hostgroup}",
     host_use      => "${templatetype}-server",
     check_command => 'check_ping!2000.00,80%!4000.00,100%!4',
     exported      => true,
   }
 
 }
+  $hostgroups = lookup('monitor::nagios::hostgroups')
+  if $hostgroups {
+    $hostgroups.each |String $hgname| {
+      nagios::resource { $hgname:
+        type     => 'hostgroup',
+        exported => false,
+      }
+    }
+  }
