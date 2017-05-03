3cfd6ca7834a46b8ef7a36649bf181f9b1b2c04c
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-05-03 13:08:24 +0930

    Remove facter directory from codeserver profile

--- site/profile/manifests/codeserver.pp
+++ site/profile/manifests/codeserver.pp
@@ -13,14 +13,6 @@ class profile::codeserver {
     group => 'root',
   }
 
-  file { ['/etc/puppetlabs/facter', '/etc/puppetlabs/facter/facts.d']:
-    ensure => 'directory',
-  }
-
-  file { '/etc/puppetlabs/facter/facts.d/facts.yaml':
-    ensure => 'present',
-  }
-
   file_line { 'applicationtier_codeservers':
     ensure => 'present',
     path   => '/etc/puppetlabs/facter/facts.d/facts.yaml',
