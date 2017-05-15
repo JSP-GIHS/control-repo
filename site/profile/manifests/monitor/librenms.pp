1b508179ee7647805ae54997731de3a9a7cdb46e
Jason SPALDING <jason.spalding@gihs.sa.edu.au>
2017-05-15 13:28:11 +0930

    Add name to database definition. Like a genius

--- site/profile/manifests/monitor/librenms.pp
+++ site/profile/manifests/monitor/librenms.pp
@@ -5,7 +5,7 @@ class profile::monitor::librenms {
   include librenms
   include librenms::nginx
 
-  mysql::db {
+  mysql::db { 'librenms_database':
     user     => 'librenms',
     password => 'V|[@7N^]}iepGRUsj"!,%2kgY%#S|,MbVvwy6rF[klatM7{f0',
     dbname   => 'librenms',
